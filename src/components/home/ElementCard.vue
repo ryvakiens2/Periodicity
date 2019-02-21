14fd8448c7b263403059d9c2a0df3f5b45e32518
Kadin Zhang <cykadin@gmail.com>
2019-02-21 17:04:02 -0500

    remove atomic mass from elementholder

--- src/components/home/ElementCard.vue
+++ src/components/home/ElementCard.vue
@@ -79,19 +79,19 @@ export default {
         font-size: 0.65vw;
         span {
             float: right;
-            color: rgba(255, 255, 255, 0.35);
+            color: rgba(255, 255, 255, 0);
             font-weight: 200;
         }
     }
     .name {
         font-size: 0.67vw;
-        margin-top: -0.2vw;
+        margin-top: -0.24vw;
         opacity: 0.9;
         font-weight: 200;
     }
     .symbol {
         font-weight: 600;
-        font-size: 2vw;
+        font-size: 1.8vw;
         line-height: 2.5vw;
     }
     &:hover {
@@ -143,66 +143,65 @@ export default {
 .alkali {
     background-color: rgba(95, 42, 53, 0.9);
     &:hover {
-        background-color: rgba(100, 50, 60, 0.4);
+        background-color: rgba(100, 50, 60, 0.5);
     }
 }
 .alkaliEarth {
     background-color: rgba(95, 70, 55, 0.9);
     &:hover {
-        background-color: rgba(93, 73, 61, 0.4);
+        background-color: rgba(93, 73, 61, 0.5);
     }
 }
 .transitionMetal {
-    background-color: rgba(50, 69, 102, 0.9);
+    background-color: rgba(58, 70, 90, 0.9);
     &:hover {
-        background-color: rgba(59, 73, 98, 0.4);
+        background-color: rgba(59, 73, 98, 0.5);
     }
 }
 .noble {
     background-color: rgba(87, 49, 126, 0.9);
     &:hover {
-        background-color: rgba(96, 60, 130, 0.4);
+        background-color: rgba(96, 60, 130, 0.5);
     }
 }
 .halogen {
     background-color: rgba(77, 74, 150, 0.9);
     &:hover {
-        background-color: rgba(102, 100, 161, 0.4);
+        background-color: rgba(102, 100, 161, 0.5);
     }
 }
 .nonMetal {
-    background-color: rgba(47, 49, 112, 0.9);
+    background-color: rgba(52, 53, 109, 0.9);
     &:hover {
-        background-color: rgba(46, 48, 108, 0.4);
+        background-color: rgba(46, 48, 108, 0.5);
     }
 }
 .metalloid {
-    background-color: rgba(29, 68, 100, 0.9);
+    background-color: rgba(21, 84, 100, 0.9);
     &:hover {
-        background-color: rgba(34, 74, 106, 0.4);
+        background-color: rgba(34, 74, 106, 0.5);
     }
 }
 .postTransition {
-    background-color: rgba(32, 94, 78, 0.9);
+    background-color: rgba(28, 100, 73, 0.9);
     &:hover {
-        background-color: rgba(34, 94, 79, 0.4);
+        background-color: rgba(34, 94, 79, 0.5);
     }
 }
 .lanthanoid {
     background-color: rgba(74, 57, 110, 0.9);
     &:hover {
-        background-color: rgba(80, 67, 111, 0.4);
+        background-color: rgba(80, 67, 111, 0.5);
     }
 }
 .actinoid {
     background-color: rgba(64, 37, 80, 0.9);
     &:hover {
-        background-color: rgba(62, 41, 73, 0.4);
+        background-color: rgba(62, 41, 73, 0.5);
     }
 }
 @media only screen and (max-width: 600px) {
     .element {
-        color: rgba(245, 245, 245, 0.55);
         width: 100%;
         height: 100%;
         box-sizing: border-box;
@@ -212,11 +211,9 @@ export default {
         transition-timing-function: ease;
         .atomicnumber {
             text-align: left;
-            color: rgba(245, 245, 245, 0.5);
             font-size: 1.2vw;
             span {
                 float: right;
-                color: rgba(245, 245, 245, 0.3);
             }
         }
         .name {
@@ -228,9 +225,6 @@ export default {
             font-weight: 600;
             font-size: 4vw;
             line-height: 4.5vw;
-        }
-        &:hover {
-            color: rgba(255, 255, 255, 0.9);
         }
     }
 }
